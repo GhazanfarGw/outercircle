05bb5bcc55ae14a29993fb32c439a556f572046f
Ghazanfar Abbas <116085344+GhazanfarGw@users.noreply.github.com>
2024-07-07 14:45:47 +0100

    Update Spotlight.jsx

--- src/home/Spotlight.jsx
+++ src/home/Spotlight.jsx
@@ -1,69 +1,7 @@
-// import React from 'react'
-
-// export default function Spotlight() {
-//   return (
-//     <>
-//         <div className='max-w-screen-xl justify-center text-center mx-auto'>
-//             <div className='mx-auto items-center md:px-10 px-5 md:py-20'>
-                // <div className='justify-center mx-auto text-center max-w-screen-md'>
-                //     <h1 className='md:text-5xl text-3xl font-semibold'>
-                //         Recruitment Spotlight
-                //     </h1>
-                //     <h1 className='md:text-5xl text-3xl font-semibold'>Apply Today!</h1>
-                //     <p className='md:text-xl md:pt-8 pt-5'>
-                //         “Discover exciting apprenticeship opening wit top-tier employers through our exclusive recruitment spotlight”
-                //     </p>
-                // </div>
-//                 <div className='md:py-20 py-8 md:pt-14'>
-//                     <div className='grid grid-cols-2 md:grid-cols-4 md:gap-5'>
-//                         <div className='w-full items-center mx-auto pt-5 px-2'>
-//                             <div className='rounded-lg border-4 hover:scale-105 duration-200 hover:border-opacity-80 border-[#1E4579] items-center text-center mx-auto'>
-//                                 <div className='md:h-36 md:w-36 md:my-10 h-24 w-24 my-5 rounded-full md:border-8 border-4 duration-200 hover:border-opacity-80 border-[#6400AB] items-center text-center mx-auto'>
-//                                     <h1 className='items-center mx-auto justify-center text-center md:py-7 py-6 text-3xl md:text-6xl font-bold text-[#6400AB]'>
-//                                         BT
-//                                     </h1>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <div className='w-full items-center mx-auto pt-5 px-2'>
-//                             <div className='rounded-lg border-4 hover:scale-105 duration-200 hover:border-opacity-80 border-[#1E4579] items-center text-center mx-auto'>
-//                                 <div className='md:h-36 md:w-36 md:my-10 h-24 w-24 my-5 rounded-full md:border-8 border-4 duration-200 hover:border-opacity-80 border-[#6400AB] items-center text-center mx-auto'>
-//                                     <h1 className='items-center mx-auto justify-center text-center md:py-7 py-6 text-3xl md:text-6xl font-bold text-[#6400AB]'>
-                                        
-//                                     </h1>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <div className='w-full items-center mx-auto pt-5 px-2'>
-//                             <div className='rounded-lg border-4 hover:scale-105 duration-200 hover:border-opacity-80 border-[#1E4579] items-center text-center mx-auto'>
-//                                 <div className='md:h-36 md:w-36 md:my-10 h-24 w-24 my-5 rounded-full md:border-8 border-4 duration-200 hover:border-opacity-80 border-[#6400AB] items-center text-center mx-auto'>
-//                                     <h1 className='items-center mx-auto justify-center text-center md:py-7 py-6 text-3xl md:text-6xl font-bold text-[#6400AB]'>
-                                        
-//                                     </h1>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <div className='w-full items-center mx-auto pt-5 px-2'>
-//                             <div className='rounded-lg border-4 hover:scale-105 duration-200 hover:border-opacity-80 border-[#1E4579] items-center text-center mx-auto'>
-//                                 <div className='md:h-36 md:w-36 md:my-10 h-24 w-24 my-5 rounded-full md:border-8 border-4 duration-200 hover:border-opacity-80 border-[#6400AB] items-center text-center mx-auto'>
-//                                     <h1 className='items-center mx-auto justify-center text-center md:py-7 py-6 text-3xl md:text-6xl font-bold text-[#6400AB]'>
-                                        
-//                                     </h1>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     </>
-//   )
-// }
-
-// src/CustomCarousel.js
 import React, { useRef } from 'react';
 import Slider from 'react-slick';
 import { NextArrow, PrevArrow } from './Arrows';
+import JobSearchBar from '../home/JobSearchBar';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -109,9 +47,9 @@ const CustomCarousel = () => {
 
   const slides = [
     { label: 'BT' },
-    { label: ' ' },
-    { label: ' ' },
-    { label: ' ' },
+    { label: 'IBM' },
+    { label: 'NSH' },
+    { label: 'ARUP' },
   ];
 
   const goToNext = () => {
@@ -131,13 +69,14 @@ const CustomCarousel = () => {
         <div className='mx-auto items-center md:px-10 px-5 md:py-20 py-10'>
             <div className='justify-center mx-auto text-center max-w-screen-md'>
                 <h1 className='md:text-5xl text-3xl font-semibold'>
-                    Recruitment Spotlight
+                  Apprenticeship Opening
                 </h1>
                 <h1 className='md:text-5xl text-3xl font-semibold'>Apply Today!</h1>
                 <p className='md:text-xl md:pt-8 pt-5'>
                     “Discover exciting apprenticeship opening wit top-tier employers through our exclusive recruitment spotlight”
                 </p>
             </div>
+            <JobSearchBar/>
             <Slider ref={sliderRef} {...settings}>
                 {slides.map((slide, index) => (
                 <div key={index} className="md:py-20 py-8 md:pt-14 px-2">
