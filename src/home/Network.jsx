af637b69c1aa7020056f595bcb6e76c6697647c3
Dan Van <116085344+GhazanfarGw@users.noreply.github.com>
2024-08-11 05:47:07 +0500

    Update Network.jsx

--- src/home/Network.jsx
+++ src/home/Network.jsx
@@ -5,17 +5,17 @@ export default function Innumber() {
   return (
     <>
     <div className='bg-[#1A1F2A]'>
-        <div className='max-w-screen-xl justify-center text-center mx-auto md:pb-72 pb-20 pt-5 md:pt-10'>
+        <div className='max-w-screen-xl justify-center text-center mx-auto md:pb-72 pb-20 pt-5 md:pt-28'>
             <div className='md:px-10 px-5 py-12'>
                 <div className='flex md:gap-8 gap-x-4 md:grid-cols-2 mx-auto justify-center items-center'>
                     <img className='w-20' src='./outercircle.png' alt='logo'/>
-                    <h1 className='md:text-5xl text-3xl font-semibold text-[#fff]'>
+                    <h1 className='md:text-6xl text-3xl font-semibold text-[#fff]'>
                         Our Network
                     </h1>
                 </div>
                 <div className='lg:grid lg:grid-cols-4 pt-5 grid grid-cols-2 mx-auto justify-center gap-3 text-center max-w-screen-lg'>
                     <div className='md:pt-20 py-4 md:px-9 px-2'>
-                        <h1 className='text-2xl lg:text-4xl font-bold justify-center text-[#fff] hover:text-[#1E4579]'>
+                        <h1 className='text-2xl lg:text-5xl font-bold justify-center text-[#fff] hover:text-[#1E4579]'>
                             <CountUp  start={1000} end={2000} duration={25.75} decimal={1} prefix="+"/>
                         </h1>
                         <p className='pt-4 text-xs md:text-base text-[#fff]'>
@@ -23,7 +23,7 @@ export default function Innumber() {
                         </p>
                     </div>
                     <div className='md:pt-20 py-4 md:px-9 px-2'>
-                        <h1 className='text-2xl lg:text-4xl font-bold justify-center text-[#fff] hover:text-[#1790E8]'>
+                        <h1 className='text-2xl lg:text-5xl font-bold justify-center text-[#fff] hover:text-[#1790E8]'>
                             <CountUp start={100} end={200} duration={25.75} decimal={1} prefix="+"/>
                         </h1>
                         <p className='pt-4 text-xs md:text-base text-[#fff]'>
@@ -31,7 +31,7 @@ export default function Innumber() {
                         </p>
                     </div>
                     <div className='md:pt-20 md:py-4 py-2 md:px-9 px-2'>
-                        <h1 className='text-2xl lg:text-4xl font-bold justify-center hover:text-[#1790E8] text-[#fff]'>
+                        <h1 className='text-2xl lg:text-5xl font-bold justify-center hover:text-[#1790E8] text-[#fff]'>
                             <CountUp start={1} end={15} duration={25.75} decimal={1} prefix="+"/>
                         </h1>
                         <p className='pt-4 text-xs md:text-base text-[#fff]'>
@@ -39,7 +39,7 @@ export default function Innumber() {
                         </p>
                     </div>
                     <div className='md:pt-20 md:py-4 py-2 md:px-9 px-2'>
-                        <h1 className='text-2xl lg:text-4xl font-bold justify-center hover:text-[#1790E8] text-[#fff]'>
+                        <h1 className='text-2xl lg:text-5xl font-bold justify-center hover:text-[#1790E8] text-[#fff]'>
                             <CountUp start={10} end={40} duration={25.75} decimal={1} prefix="+"/>
                         </h1>
                         <p className='pt-4 text-xs md:text-base text-[#fff]'>
